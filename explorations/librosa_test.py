ab030126c8b30b6edab8c26e1b60c27f33638f05
Qiushan_Linux <zhao940089273@163.com>
2022-11-17 13:15:05 -0500

    pitch

--- explorations/librosa_test.py
+++ explorations/librosa_test.py
@@ -1,2 +1,9 @@
 import librosa
-librosa.show_versions()
+import numpy as np
+# librosa.show_versions()
+# librosa.hz_to_note(444.0)
+rst = librosa.hz_to_note(440.0)
+basic_pitch_freq = 440.0 * (2.0 ** np.linspace(0, 1, 12))
+basic_pitch_note = librosa.hz_to_note(basic_pitch_freq)
+print(basic_pitch_note)
+print(librosa.note_to_hz(basic_pitch_note))
--- source/pitch_detection.py
+++ source/pitch_detection.py
@@ -1,3 +1,35 @@
 """
 Functions related to pitch detection.
 """
+import librosa
+import matplotlib.pyplot as plt
+import numpy as np
+import statsmodels.api as sm
+from scipy.signal import find_peaks
+import soundfile
+import os
+
+# Single Pitch Detection
+
+def pitch_detection(file_path):
+    # load a file, get the music and the sample_freq
+    data, sample_freq = librosa.load(soundfile.SoundFile(file_path))
+    T = 1 / sample_freq
+    N = len(data)
+
+    Y_k = np.fft.fft(data)[0:int(N/2)]/N # FFT
+    Y_k[1:] = 2*Y_k[1:] # Single-sided spectrum
+    Pxx = np.abs(Y_k) # Power spectrum
+
+    f = sample_freq * np.arange((N/2)) / N; # frequencies
+
+    # plotting
+    fig,ax = plt.subplots()
+    plt.plot(f[0:5000], Pxx[0:5000], linewidth=2)
+    plt.ylabel('Amplitude')
+    plt.xlabel('Frequency [Hz]')
+    plt.show()(path)
+
+
+pitch_freq = pitch_detection("/home/zhao/Documents/automatic_music_notation/sound_track/dou1.mp3")
+print(pitch_freq)
