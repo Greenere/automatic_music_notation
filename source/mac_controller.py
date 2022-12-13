26f8204a307ecfd0bc0eae717a4cfcd48e058839
Greenere <2897643352@qq.com>
2022-12-13 13:27:06 -0500

    add the website template

---
+++ source/mac_controller.py
@@ -0,0 +1,19 @@
+def start_pressed():
+    if input("CMD") == "s":
+        return True
+    return False
+
+def pause_pressed():
+    if input("CMD") == "p":
+        return True
+    return False
+
+def quit_pressed():
+    if input("CMD") == "q":
+        return True
+    return False
+
+def end_pressed():
+    if input("CMD") == "e":
+        return True
+    return False
